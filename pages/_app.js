e4105f8ecb329528a689408ee613c181525e4fbe
OlaoyeVic <you@example.com>
2022-07-12 09:49:18 +0100

    fetching data with graphql

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
+import { ApolloProvider } from '@apollo/client'
+import Head from 'next/head'
+import { useApollo } from '../lib/apollo'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const apolloClient = useApollo(pageProps.initialApolloState)
+  
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <link href='https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css' rel='stylesheet' />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,66 @@
 import Head from "next/head"
 import Link from "next/link"
 import axios from "axios"
+import { useEffect, useState } from "react"
+import { useQuery } from "@apollo/client"
+import GET_LATEST_SIGNS from "../lib/apollo/queries/getLatestSigns"
+import Sign from "../components/Sign"
+import Loading from "../components/Loading"
 
-
-export const getServerSideProps = async() => {
-  const usersReq = await axios.get('https://api.rwnjs.com/04/users')
-  console.log(usersReq.data)
-  return {
-    props: {
-      users: usersReq.data
-    }
-  }
-}
 
 // export const getServerSideProps = async() => {
-//   const response = await axios.get('https://trip-buddy-be.herokuapp.com/api/v1/tripbuddy')
-//   console.log(response.data)
+//   const usersReq = await axios.get('https://api.rwnjs.com/04/users')
+//   console.log(usersReq.data)
 //   return {
 //     props: {
+//       users: usersReq.data
 //     }
 //   }
 // }
 
 const HomePage = ({users}) => {
+  const { loading, data } = useQuery(GET_LATEST_SIGNS, {
+    fetchPolicy: 'no-cache'
+  })
+
+  if(loading) {
+    return <Loading />
+  }
+
   return (
-    <ul>
-      {users?.map((user) =>
-        <li key={user.id}>
-          <Link href={`/users/${username}`} passHref>
-            <a>{user.username}</a>
-          </Link>
-        </li>
-      )}
-    </ul>
+    <div className="flex justify-center items-center flex-col mt-20">
+      <h1 className="text-3xl mb-5">Real World Next.js Signbook</h1>
+      <Link href='/new-sign'>
+        <button className="mb-8 border-2 border-purple-800 text-purple-900 p-2 rounded-lg text-gray-50 m-auto mt-4">
+          Add new Sign
+        </button>
+      </Link>
+      <div>
+        {data && data?.sign.map((sign) => (
+          <Sign key={sign.uuid} {...sign} />
+        ))}
+      </div>
+    </div>
   )
 }
 export default HomePage
+
+// const Users = () => {
+//   const [loading, setLoading] = useState(true)
+//   const [data, setData] = useState(null)
+
+//   useEffect(async () => {
+//     const req = await axios.get('https://api.rwnjs.com/04/users')
+//     // const users = await req.json()
+
+//     setLoading(false)
+//     setData(users)
+//   }, [])
+
+//   return (
+//     <div>
+//      {loading && <div>Loading users ...</div>}
+//      {loading && <HomePage users={data} />}
+//     </div>
+//   )
+// }
