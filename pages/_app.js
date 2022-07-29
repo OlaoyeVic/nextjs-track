dcf8b3cf3892c9d187be2610e5558be674294bf7
OlaoyeVic <you@example.com>
2022-07-29 12:14:01 +0100

    implemented headless-ui with next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
 import Head from 'next/head'
 import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
-import { ThemeProvider } from 'next-themes'
-import TopBar from '../components/TopBar'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider attribute='class'>
-      <div className="dark:bg-gray-900 bg-gray-50 w-full min-h-screen">
-        <TopBar />
-        <Component {...pageProps} />
-      </div>
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
 export default MyApp
