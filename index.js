fd5dd0b3d45e0b6711eb1e766567434618f4bc7e
OlaoyeVic <you@example.com>
2022-08-11 03:11:22 +0100

    using a custom fastify server

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-const { parse } = require('url')
-const express = require('express')
-const next = require('next')
-const { url } = require('inspector')
+const fastify = require('fastify') ()
 
+fastify
+    .register(require('fastify-nextjs'))
+    .after( () => {
+        fastify.next('/')
+        fastify.next('/about')
+        fastify.next('/greet/user')
+        fastify.get('/contacts/', (req, reply) => {
+            reply
+                .type('html')
+                .send('<h1>Contacts Page</h1>')
+        })
+    })
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-
-async function main() {
-    try {
-        await app.prepare()
-
-        const handle = app.getRequestHandler()
-        const server = express()
-
-        server
-            .get('*', (req, res) => {
-                const url = parse(req.url, true)
-                handle(req, res, url)
-            })
-            .get('/', (req, res) => {
-                res.send('hello world!')
-            })
-            .get('/about', (req, res) => {
-                const { query } = parse(req.url, true)
-                app.render(req, res, '/about', query)
-            })
-            .get('/api/greet', (req, res) => {
-                res.json({ name: req.query?.name ?? 'unknown'})
-            })
-            .get(/_next\/.+/, (req, res) => {
-                const parsedUrl = parse(req.url, true)
-                handle(req, res, parsedUrl)
-            })
-            .listen(3000, () => console.log('server ready'))
-    } catch (err) {
-        console.log(err.stack)
-    }
-}
-
-main()
+fastify.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000')
+})
