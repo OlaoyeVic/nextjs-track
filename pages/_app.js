ae81efa66f2e24ac91db799ce5a7595b54a61aca
OlaoyeVic <you@example.com>
2022-08-12 11:31:37 +0100

    unit and integration testing with jest and react-testing-library

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-full min-h-screen bg-gray-100">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,5 @@
 import NextDocument, {Html, Head, Main, NextScript} from "next/document";
-import { ColorModeScript, extendTheme } from '@chakra-ui/react'
 
-const config = {
-    useSystemColorMode: true
-}
-
-const theme = extendTheme({ config })
 
 class MyDocument extends NextDocument {
     render () {
@@ -13,7 +7,6 @@ class MyDocument extends NextDocument {
             <Html lang="en">
                 <Head />
                 <body>
-                    <ColorModeScript initialColorMode={theme.config.ColorModeScript} />
                     <Main />
                     <NextScript />
                 </body>
