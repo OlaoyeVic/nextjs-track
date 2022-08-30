fb23572ad16e678160d8915d5721a19766734416
OlaoyeVic <you@example.com>
2022-08-30 14:38:59 +0100

    implemented authentication with auth0

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,31 @@
 import styles from '../styles/app.module.css'
+import { useUser } from '@auth0/nextjs-auth0';
 
-export default function Home() {
+export default function Index() {
+  const { user, error, isLoading } = useUser()
+
+  if(isLoading) return <p>Loading ...</p>
+
+  if(error) {
+    return <div>{error.message}</div>
+  }
+
+  if(user) {
+    return (
+      <div>
+        <h1>Welcome back!</h1>
+        <p>
+          You're logged in with the following email address: {user.email}!
+        </p>
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Homepage</h1>
+      <h1>Welcome stranger!</h1>
+      <p>Please <a href='/api/auth/login'>Login</a>.</p>
     </div>
   );
 }
